1cc85bed9b30eb04f38c88ab6159e77678e6053d
kingofmidas <kingofmidas@protonmail.com>
2019-10-01 20:59:24 +0300

    some updates

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
@@ -24,6 +24,8 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+
+    # RESTAURANT
     url(r'^restaurant/$', views.restaurant_home, name="restaurant-home"),
 
     url(r'^restaurant/sign-in/$', auth_views.login,
@@ -32,7 +34,19 @@ urlpatterns = [
     url(r'^restaurant/sign-out', auth_views.logout,
         {'next_page': '/'}, name='restaurant-sign-out'),
     
-     url(r'^restaurant/sign-up', views.restaurant_sign_up,
+    url(r'^restaurant/sign-up', views.restaurant_sign_up,
         name='restaurant-sign-up'),
+
+    url(r'^restaurant/account/$', views.restaurant_account, name='restaurant-account'),
+    url(r'^restaurant/meal/$', views.restaurant_meal, name='restaurant-meal'),
+    url(r'^restaurant/meal/add/$', views.restaurant_add_meal, name='restaurant-add-meal'),
+    url(r'^restaurant/meal/edit/(?P<meal_id>\d+)/$', views.restaurant_edit_meal, name='restaurant-edit-meal'),
+    url(r'^restaurant/order/$', views.restaurant_order, name='restaurant-order'),
+    url(r'^restaurant/report/$', views.restaurant_report, name='restaurant-report'),
+
+    #Sign In/ Sign Up/ Sign Out
+    url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
+    # convert-token (sign in/ sign up)
+    # revoke-token (sign out)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import *
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
+admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
