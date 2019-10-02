4b73395da48a6154084de626596583e14f6f5113
kingofmidas <kingofmidas@protonmail.com>
2019-10-02 11:20:59 +0300

    create rest api

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,25 +1,11 @@
-"""foodtasker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
-from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
-
 from django.conf.urls.static import static
 from django.conf import settings
+
+from foodtaskerapp import views, apis
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -48,5 +34,7 @@ urlpatterns = [
     url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
     # convert-token (sign in/ sign up)
     # revoke-token (sign out)
+
+    url(r'^api/customer/restaurants/$', apis.customer_get_restaurants),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
